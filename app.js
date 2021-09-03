7febd192f0f890a4258e4c4468e0f8cf7115538c
justin-hill426 <jrhill426@gmail.com>
2021-09-03 09:12:19 -0600

    Added proper error handling routes

--- app.js
+++ app.js
@@ -34,6 +34,25 @@ app.get("/project/:id", (req, res, next) => {
     }
 });
 
+app.use((req, res, next) => {
+    console.error("404 error handler called...");
+    const err = new Error();
+    err.status = 404;
+    err.message = "Looks like this page does not exist...";
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.error("Global error handler called");
+    if (err.status === 404) {
+        console.log("404 error... it looks like this page doesn't exist please try again with a different URL")
+    } else {
+        err.message = err.message || "Something went wrong on the server";
+        console.log(err.message)
+    }
+    next();
+});
+
 app.listen(process.env.PORT || 3000, () =>
     console.log("App is running on port 3000.")
 );
